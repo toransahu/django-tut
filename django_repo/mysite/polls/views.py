a0ee9919256b4318b588535723b681d0c8021f58
toransahu <toran.sahu@yahoo.com>
2017-06-06 16:15:45 +0530

    added 404 handling, removed hardcoded URLs in templates, namespaced URL names

--- django_repo/mysite/polls/views.py
+++ django_repo/mysite/polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-#from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 #from django.template import loader
 from .models import Question
+from django.http import Http404
 
 # Create your views here.
 
@@ -11,7 +12,7 @@ def index(request):
     latest_question_list= Question.objects.order_by('-pub_date')[:5]
     output=','.join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
-    
+
 
 #to use template
 def index(request):
@@ -30,10 +31,22 @@ def index(request):
         'latest_question_list':latest_question_list,
         }
     return render(request,'polls/index.html',context)
-
-
+'''
+#1. added Http404
+#2. removed HttpResponse, used render shortcut & added detail.html template
 def details(request,question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        #1.
+        raise Http404("Question does not exist")
+    #2 return HttpResponse("You're looking at question %s." % question_id)
+    return render(request, 'polls/detail.html',{'question':question})
+'''
+# used shortcut for http404 handling "get_object_or_404"
+def details(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/detail.html',{'question':question})
 
 def results(request, question_id):
     response= "You're looking at the results of question %s."
