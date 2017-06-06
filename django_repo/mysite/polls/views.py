ebe4029fcbb3d0ed37d112d38ed1a7a105d1b41d
Toran Sahu <toran.sahu@yahoo.com>
2017-06-06 14:41:18 +0530

    modified polls app

--- django_repo/mysite/polls/views.py
+++ django_repo/mysite/polls/views.py
@@ -1,16 +1,42 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+#from django.http import HttpResponse
+#from django.template import loader
+from .models import Question
 
 # Create your views here.
 
+'''
+#normal
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+    latest_question_list= Question.objects.order_by('-pub_date')[:5]
+    output=','.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+    
+
+#to use template
+def index(request):
+    latest_question_list= Question.objects.order_by('-pub_date')[:5]
+    template=loader.get_template('polls/index.html')
+    context ={
+        'latest_question_list':latest_question_list,
+        }
+    return HttpResponse(template.render(context,request))
+'''
+
+#Shortcut: to use template & render() (no need to import HttpResponse & loader
+def index(request):
+    latest_question_list= Question.objects.order_by('-pub_date')[:5]
+    context ={
+        'latest_question_list':latest_question_list,
+        }
+    return render(request,'polls/index.html',context)
+
 
 def details(request,question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    resnponse= "You're looking at the results of question %s."
+    response= "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
