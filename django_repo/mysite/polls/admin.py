e793a3e126661d27b9949f8ebf23fe5ce8ede1b2
Toran Sahu <toran.sahu@yahoo.com>
2017-08-08 13:16:23 +0530

    customized: re-ordered pub_date and question_text fields

--- django_repo/mysite/polls/admin.py
+++ django_repo/mysite/polls/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Question
 # Register your models here.
 
-admin.site.register(Question)
+#customize the admin form
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+    #This particular change above makes the “Publication date”
+    #come before the “Question” field
+
+admin.site.register(Question, QuestionAdmin)
